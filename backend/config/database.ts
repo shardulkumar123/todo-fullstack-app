23b61ae6d470e519dbb1e276405f8d3a0766522d
Shardul Kumar <kumarshardul1998@gmail.com>
2025-03-05 04:51:30 +0530

    fix: remove commented code

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,25 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
-import User from "../models/userSchema";  // Import User model
-import Item from "../models/itemSchema";  // Import Item model
+import User from "../models/userSchema";
+import Item from "../models/itemSchema";
 
 dotenv.config();
-
-// export const sequelize = new Sequelize({
-//     database: process.env.POSTGRES_DB || "your_database",
-//     username: process.env.POSTGRES_USER || "your_username",
-//     password: process.env.POSTGRES_PASSWORD || "your_password",
-//     host: process.env.POSTGRES_HOST || "127.0.0.1",
-//     dialect: "postgres",
-//     logging: false, // Set to `console.log` if you want to log queries
-//     models: [User, Item], // Register models here
-// });
-
-// export const sequelize = new Sequelize('myDb', 'postgres', '1234', {
-//     host: 'localhost',
-//     dialect: "postgres",
-//     models: [__dirname + '../models'], // Register models here
-// });
 
 export const sequelize = new Sequelize({
     database: process.env.POSTGRES_DB,
@@ -28,17 +12,16 @@ export const sequelize = new Sequelize({
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",  // Ensure dialect is 'postgres'
     logging: false,
-    // logging: console.log,
     models: [User, Item]
 });
 
 export const connectPostgres = async () => {
     try {
         await sequelize.authenticate();
-        console.log("✅ PostgreSQL connected successfully");
+        console.log("PostgreSQL connected successfully");
 
-        await sequelize.sync({ alter: true }); // Sync models without dropping tables
+        await sequelize.sync({ alter: true });
     } catch (error) {
-        console.error("❌ PostgreSQL connection error:", error);
+        console.error("PostgreSQL connection error:", error);
     }
 };
